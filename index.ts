475e7d5a2f4501eeec8d1f4a216d962957dcc5f4
naiselim <452075061@qq.com>
2023-03-17 16:39:26 +0800

    更换背景图，效果确定

--- index.ts
+++ index.ts
@@ -7,11 +7,11 @@ var createScene = function () {
 	light.diffuse = new BABYLON.Color3(1, 0, 0);
 	
 	// Skybox
-	var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
+	var skybox = BABYLON.MeshBuilder.CreateSphere("skyBox", {diameter:50.0}, scene);
 	var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
 	skyboxMaterial.backFaceCulling = false;
-	skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/skybox", scene);
-	skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+	skyboxMaterial.reflectionTexture = new BABYLON.Texture("https://raw.githubusercontent.com/Naiselim/WeddingShowcase/master/Background.jpg", scene);
+	skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.PROJECTION_MODE;
 	skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
 	skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
 	skybox.material = skyboxMaterial;			
@@ -22,10 +22,10 @@ var createScene = function () {
         meshes[0].rotation = new BABYLON.Vector3(-Math.PI / 2, 0, 0);
         scene.registerBeforeRender(()=>{
             number += 1;
-            if(number == 30)meshes[0].position.y = -0.5;
+            if(number == 30)meshes[0].position.y = -1.0;
             else if(number == 60){
                 number = 0;
-                meshes[0].position.y = 0;
+                meshes[0].position.y = -0.5;
             }
         });
     });    
